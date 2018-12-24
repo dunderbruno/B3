1c220fce6a44f90e559fc4c0772ed3b5e3d500e3
Bruno Santos <brunosantos@protonmail.com>
2018-12-24 06:39:41 -0300

    Lê os dados de um arquivo e salva no banco

---
+++ txttobd.py
@@ -0,0 +1,16 @@
+u"""Lê arquivo e grava no banco de dados."""
+
+import sqlite3
+
+conn = sqlite3.connect('b3.db')
+cursor = conn.cursor()
+
+with open('codigos.txt', 'r') as codigos:
+    lista = codigos.readlines()
+
+for i in range(0, len(lista), 2):
+    command = 'INSERT INTO empresas (name, code) VALUES (?, ?)'
+    cursor.execute("""%s""" % command, (lista[i][:-1], lista[i+1][:-1]))
+
+conn.commit()
+conn.close()
