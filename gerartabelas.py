a7384fd93451ea18a947ff07d46f5af4829fe09e
Bruno Santos <brunosantos@protonmail.com>
2018-12-25 15:26:36 -0300

    inclui coluna data

--- gerartabelas.py
+++ gerartabelas.py
@@ -1,18 +1,22 @@
 """Gerar tabelas no banco de dados."""
 
 import sqlite3
+import time
 
+inicio = time.time()
 
 conn = sqlite3.connect('b3.db')
 cursor = conn.cursor()
 
 nomes = cursor.execute("""SELECT code FROM empresas""")
 for i in nomes.fetchall():
-    command = 'CREATE TABLE %s (flag INTEGER, var REAL, var_percentual REAL, ultima REAL, maxima REAL, minima REAL, abertura REAL, volume INTEGER)' % i[0][:-3]
-    # print(command)
+    command = 'CREATE TABLE %s (id PRIMARYKEY, data TEXT, resultado INTEGER, var REAL, var_percentual REAL, ultima REAL, maxima REAL, minima REAL, abertura REAL, volume INTEGER)' % i[0][:-3]
     cursor.execute("""%s""" % command)
     print(i[0])
-# cursor.execute("""CREATE TABLE ships (race text, alive real)""")
 
 conn.commit()
 conn.close()
+
+fim = time.time()
+
+print((fim-inicio)/60)
--- preencher.py
+++ preencher.py
@@ -1,20 +1,20 @@
 """Preenche a tabela."""
+import sqlite3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import sqlite3
 from datetime import date
+import time
 
+inicio = time.time()
 today = date.today()
 
 conn = sqlite3.connect('b3.db')
 cursor = conn.cursor()
 
-codigos = cursor.execute("""SELECT code
-                  FROM empresas""")
-
-# print(codigos.fetchall())
+codigos = cursor.execute("""SELECT code FROM empresas""")
 
 urlbase = 'http://cotacoes.economia.uol.com.br/acao/index.html?codigo='
+hoje = str(date.today())
 for i in codigos.fetchall():
     print(i[0])
     html = urlopen(urlbase+i[0])
@@ -23,10 +23,14 @@ for i in codigos.fetchall():
     dados = [i.getText() for i in body]
     if len(dados) > 1:
         del(dados[0])
+        dados = [hoje] + dados
         print(dados)
-        command = 'INSERT INTO %s (var, var_percentual, ultima, maxima, minima, abertura, volume) VALUES (?, ?, ?, ?, ?, ?, ?)' % i[0][:-3]
+        command = 'INSERT INTO %s (data, var, var_percentual, ultima, maxima, minima, abertura, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?)' % i[0][:-3]
         cursor.execute("""%s""" % command, dados)
 
 
 conn.commit()
 conn.close()
+fim = time.time()
+
+print((fim-inicio)/60)
