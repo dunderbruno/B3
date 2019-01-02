3f20b3bfbcf1b927f05cbfa107fc8f9126f18ce9
Bruno Santos <brunosantos@protonmail.com>
2019-01-02 08:52:07 -0300

    refactor

--- preencher.py
+++ preencher.py
@@ -1,10 +1,14 @@
-"""Preenche a tabela."""
+"""
+Preenche a tabela.
+
+Consulta os dados e salva nas respectivas tabelas.
+"""
+
+import os
 import sqlite3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import date
-import time
-import os
 from threading import Thread
 
 
@@ -12,26 +16,24 @@ conn = sqlite3.connect('b3.db', check_same_thread=False)
 cursor = conn.cursor()
 codigos = cursor.execute("""SELECT code FROM empresas""").fetchall()
 conn.close()
-urlbase = 'http://cotacoes.economia.uol.com.br/acao/index.html?codigo='
-hoje = str(date.today())
+url = 'http://cotacoes.economia.uol.com.br/acao/index.html?codigo='
 
 iteracoes = 0
 
+
 def getvalues(start):
     for i in range(start, int(len(codigos)), 4):
-        # print('Thread %d' % start)
         os.system('clear')
         global iteracoes
         iteracoes += 1
         print('Baixados %d / 1974.' % iteracoes)
-        html = urlopen(urlbase+codigos[i][0])
+        html = urlopen(url+codigos[i][0])
         bsObj = BeautifulSoup(html.read(), 'html5lib')
         body = bsObj.findAll("td")
         dados = [j.getText() for j in body]
         if len(dados) > 1:
             del(dados[0])
-            dados = [hoje] + dados
-            # print(dados)
+            dados = [str(date.today())] + dados
             conn = sqlite3.connect('b3.db', check_same_thread=False)
             cursor = conn.cursor()
             command = 'INSERT INTO %s (data, var, var_percentual, ultima, maxima, minima, abertura, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?)' % codigos[i][0][:-3]
